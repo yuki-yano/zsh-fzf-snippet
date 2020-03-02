54ab629191e62944739ab3920ee2e94de245e278
yuki_yano <yuki.ycino@gmail.com>
2020-03-02 21:59:17 +0900

    config editor

--- zsh-fzf-snippet.zsh
+++ zsh-fzf-snippet.zsh
@@ -1,4 +1,5 @@
 FZF_SNIPPET_CONFIG_DIR=${FZF_SNIPPET_CONFIG_DIR:-"$HOME/.config/fzf-snippets"}
+FZF_SNIPPET_EDITOR=${EDITOR:-vim}
 
 function fzf-snippet-selection() {
   local out snippet key file
@@ -9,7 +10,7 @@ function fzf-snippet-selection() {
   if [[ $snippet != "" ]]; then
     if [[ $key == "ctrl-e" ]]; then
       file=$(echo $snippet | cut -d' ' -f 1)
-      vi $FZF_SNIPPET_CONFIG_DIR/$file
+      eval "$FZF_SNIPPET_EDITOR $FZF_SNIPPET_CONFIG_DIR/$file"
     else
       snippet=$(echo $snippet | awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}')
       BUFFER=$snippet
