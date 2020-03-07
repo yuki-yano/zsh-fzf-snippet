438c5f9bf27fcfcd2fc5e26cd08d68aa85f29561
yuki_yano <yuki.ycino@gmail.com>
2020-03-07 16:59:09 +0900

    Add extra args

--- zsh-fzf-snippet.zsh
+++ zsh-fzf-snippet.zsh
@@ -1,15 +1,20 @@
 FZF_SNIPPET_CONFIG_DIR=${FZF_SNIPPET_CONFIG_DIR:-"$HOME/.config/fzf-snippets"}
 FZF_SNIPPET_EDITOR=${EDITOR:-vim}
+FZF_SNIPPET_EXTRA_ARG=${FZF_SNIPPET_EXTRA_ARG:-''}
 
 function fzf-snippet-selection() {
-  local out snippet key file
+  local out snippet key file candidate fzf_command command
 
   if [[ ! -f $FZF_SNIPPET_CONFIG_DIR/snippets ]]; then
     echo 'snippets file does not exist' >&2
     exit 1
   fi
 
-  IFS=$'\n' out=($(\grep -v "^#" $FZF_SNIPPET_CONFIG_DIR/snippets | \grep -v "^\s*$" | fzf --query "$LBUFFER" --prompt="Snippet> " --preview="cat $FZF_SNIPPET_CONFIG_DIR/{1}" --expect=ctrl-e))
+  grep_command='\grep -v "^#" $FZF_SNIPPET_CONFIG_DIR/snippets | \grep -v "^\s*$"'
+  fzf_command='fzf --query "$LBUFFER" --prompt="Snippet> " --preview="cat $FZF_SNIPPET_CONFIG_DIR/{1}" --expect=ctrl-e $FZF_SNIPPET_EXTRA_ARG'
+  command="$grep_command | $fzf_command"
+
+  IFS=$'\n' out=$(eval $command)
   key=$(echo "$out" | head -1)
   snippet=$(echo "$out" | head -2 | tail -1)
 
